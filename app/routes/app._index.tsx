1f2add1a4ec944a25b5874db36173536cbf6384d
divyarajsinhjadeja07 <155974577+divyarajsinhjadeja07@users.noreply.github.com>
2026-04-07 15:31:47 +0530

    Apply Surcharge on Blend Collection

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -233,11 +233,11 @@ export default function Index() {
             )}
             {targetProduct ? (
               <s-paragraph>
-                Target Collection: <b style={{ color: "green" }}>Active (ID: 446838440227)</b>
+                Target Collections: <b style={{ color: "green" }}>Active (IDs: 446838440227, 701490528643)</b>
               </s-paragraph>
             ) : (
               <s-paragraph>
-                Target Collection: <b style={{ color: "green" }}>Active (ID: 446838440227)</b>
+                Target Collections: <b style={{ color: "green" }}>Active (IDs: 446838440227, 701490528643)</b>
               </s-paragraph>
             )}
             <s-paragraph>
